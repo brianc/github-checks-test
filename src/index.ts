ec4c6bb3c545e128d4391e7abc9853b2a24721d1
bmc <brian@pecanware.com>
2020-06-30 15:41:46 +0000

    testing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import express from 'express'
 
 const app = express()
 
+app.use((req, res, next) => {
+  console.log('req', req.method, req.url)
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
