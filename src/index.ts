9822294218870715f25b9336a42c153661fef765
bmc <brian@pecanware.com>
2020-06-30 15:44:06 +0000

    asdf

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { json } from 'body-parser'
 
 const app = express()
+app.use(json())
 
 app.use((req, res, next) => {
   console.log('req', req.method, req.url)
+  console.log('body', req.body)
   next()
 })
 
